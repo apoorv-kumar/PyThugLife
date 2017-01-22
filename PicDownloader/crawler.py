aae93816d51c3369e525eb5132fa37b04ee5db84
Apoorv Kumar <apoorv.kumar247@gmail.com>
2017-01-22 23:34:17 +0530

    add test for pic download.

--- PicDownloader/crawler.py
+++ PicDownloader/crawler.py
@@ -38,7 +38,7 @@ class Crawler:
     @classmethod
     def download_pic(cls, pic_url):
         response = requests.get(pic_url, stream=True)
-        name = str(hash(pic_url)&65535) + ".jpg"
+        name = str(hash(pic_url + str(time()))&65535) + ".jpg"
         image_loc = cls.get_base_dir() + '/' + name
         with open(image_loc, 'wb') as out_file:
             shutil.copyfileobj(response.raw, out_file)
--- PicDownloader/test_crawler.py
+++ PicDownloader/test_crawler.py
@@ -1,12 +1,18 @@
 import unittest
 from crawler import Crawler
 import os
+import sys
+from unittest.mock import patch
+from io import BytesIO
 
 class SampleCrawler(Crawler):
 
     @staticmethod
     def get_base_dir():
-        return 'C:/Users/apoor/AppData/Local/Temp/'
+        if sys.platform == 'linux':
+            return '/tmp/'
+        else:
+            return 'C:/Users/apoor/AppData/Local/Temp/'
 
     @staticmethod
     def get_image_lambda():
@@ -16,9 +22,16 @@ class SampleCrawler(Crawler):
     def get_link_lambda():
         return lambda link: "__isvalidlink__" in link
 
-class CrawlTest(unittest.TestCase):
 
+class CrawlTest(unittest.TestCase):
     junk_files = []
+
+    def mocked_requests_get(*args, **kwargs):
+        class MockedResponse:
+            def __init__(self):
+                self.raw = BytesIO(b"response_content")
+
+        return MockedResponse()
 
     def test_url_logger(self):
         written_urls = ['http://some_url.com', 'https://some_url.com']
@@ -33,9 +46,19 @@ class CrawlTest(unittest.TestCase):
             self.assertTrue(written_url in read_urls)
         self.assertTrue(len(written_urls) == len(written_urls))
 
+    @patch('requests.get', side_effect=mocked_requests_get)
+    def test_download_pic(self, mock_get):
+        img_loc = SampleCrawler.download_pic("http://test_url")
+        self.junk_files.append(img_loc)
+        with open(img_loc, 'r') as img_file:
+            img_data = img_file.read()
+        print("this", img_data)
+        self.assertEqual(img_data, "response_content")
 
-    def tearDown(self):
-        for junk_file in self.junk_files:
+
+    @classmethod
+    def tearDownClass(cls):
+        for junk_file in cls.junk_files:
             os.remove(junk_file)
 
 if __name__ == '__main__':
